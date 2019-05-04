7184be161e48aeb89cf0da0cee066f995bc80505
Christian Norrman <christian@faxity.se>
2019-05-04 15:54:35 +0200

    Updated dice v2 game view

--- view/dice/game.php
+++ view/dice/game.php
@@ -2,14 +2,16 @@
 
 <?php include "icons.svg"; ?>
 
-<?= $game->render(); ?>
+<h1>Dice 100 v2</h1>
 
 <p>Din poäng: <?= $player->score(); ?></p>
 <p>Serverns poäng: <?= $cpu->score(); ?></p>
 
-<form action="" method="POST">
+<form class="dice-form" action="" method="POST">
     <button type="submit" name="action" value="toss">Slå tärningarna</button>
     <?php if ($player->turnScore() > 0) : ?>
-    <button type="submit" name="action" value="end_turn">Avsluta rundan</button>
+    <button class="red" type="submit" name="action" value="end_turn">Avsluta rundan</button>
     <?php endif; ?>
 </form>
+
+<?= $game->render(); ?>
